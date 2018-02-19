356bd1d81cac1b451797df6ae2319eea1a543e27
Jekin <j.sanghavi12@gmail.com>
2018-02-18 19:25:58 -0500

    Merge overlapping Intervals

--- CodeRust/Arrays/MergeIntervals.cs
+++ CodeRust/Arrays/MergeIntervals.cs
@@ -4,20 +4,14 @@ using System.Linq;
 namespace CodeRust.Arrays
 {
     public class MergeIntervals
-    {
+    {   //https://www.geeksforgeeks.org/merging-intervals
         public static List<List<int>> Merge(List<List<int>> input)
         {
-            List<List<int>> results = new List<List<int>>();
             input.OrderBy(ip => ip.First()).ThenBy(ip=>ip.Last());
             for (int i = 0; i < input.Count-1; i++)
             {
-                var firstFirst = input[i].First();
-                var firstLast = input[i].Last();
-                var secondFirst = input[i+1].First();
-                var secondLast = input[i+1].Last();
-                if(firstFirst <= secondLast && firstLast >= secondFirst)
+                if(input[i].First() <= input[i + 1].Last() && input[i].Last() >= input[i + 1].First())
                 {
-                    //results.Add(new List<int>{input[i].First(), input[i+1].Last()});
                     input[i + 1][0] = input[i].First();
                     input.RemoveAt(i);
                 }
