704a7f666e25e7897e36d7212bc7ce399d219c88
Jeks19 <j.sanghavi12@gmail.com>
2018-08-18 23:40:00 -0400

    Connect same level nodes:

---
+++ CodeRust/BinaryTrees/ConnectSameLevelNodes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeRust.BinaryTrees
+{
+    public class ConnectSameLevelNodes
+    {
+        public static void LevelOrder<T>(BinaryTreeNode<T> root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            Queue<BinaryTreeNode<T>> queue1 = new Queue<BinaryTreeNode<T>>();
+            Queue<BinaryTreeNode<T>> queue2 = new Queue<BinaryTreeNode<T>>();
+            Queue<BinaryTreeNode<T>> queue3 = new Queue<BinaryTreeNode<T>>();
+            queue2.Enqueue(root);
+            BinaryTreeNode<T> previous = null;
+            while (queue2.Count > 0)
+            {
+                while (queue2.Count > 0)
+                {
+                    queue1.Enqueue(queue2.Dequeue());
+                }
+               
+                previous = null;
+                while (queue1.Count > 0)
+                {
+                    var current = queue1.Dequeue();
+                    if (previous == null)
+                    {
+                        previous = current;
+                        queue3.Enqueue(previous);
+                    }
+                    else {
+                        previous.Right = current;
+                        previous = current;
+                    }
+                    if (current.Left != null)
+                    {
+                        queue2.Enqueue(current.Left);
+                    }
+                    if (current.Right != null)
+                    {
+                        queue2.Enqueue(current.Right);
+                    }
+                    previous.Right = null;
+                }
+            }
+           
+            while(queue3.Count > 0 ){
+                var curr = queue3.Dequeue();
+                StringBuilder sb = new StringBuilder();
+                while(curr != null) {
+                    sb.Append(curr.Value + "=> ");
+                    curr = curr.Right;
+                }
+                Console.WriteLine(sb.ToString().Substring(0, sb.Length - 3));
+            }
+
+        }
+    }
+}
